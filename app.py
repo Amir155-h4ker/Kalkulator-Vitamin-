7746b75a50e1fc893dd70d2f65d8b9c7e8db9543
Amir Nur Rauf <madmaxi871020@gmail.com>
2025-05-22 21:25:54 +0700

    kalku vitamin app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import pandas as pd
 
+st.markdown("""
+    <style>
+    .stApp {
+        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
+                    url('https://images.app.goo.gl/DvFnScyNVMxJ7uwUA ');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        color: white !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Konfigurasi halaman
 st.set_page_config(page_title="Kalkulator Vitamin MPASI", layout="wide")
 
