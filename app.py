3888ceeb8aea37cc0d3028f6e1b260c7102c5809
Amir Nur Rauf <madmaxi871020@gmail.com>
2025-05-22 21:41:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.markdown("""
     <style>
     .stApp {
         background: linear-gradient(rgba(0, 0, 0, 0.80), rgba(0, 0, 0, 0.80)),
-                    url('https://img.freepik.com/free-photo/person-paying-using-nfc-technology_23-2149893740.jpg');
+                    url('https://images.app.goo.gl/Kv61tsNkuJr4X14N7');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
